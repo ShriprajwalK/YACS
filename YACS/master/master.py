184101e2d56323e73812f4a561e2c0a69c8e1290
Prajwal <shriprajwalk@gmail.com>
2020-11-30 16:50:40 +0530

    Esatablish basic communication

--- YACS/master/master.py
+++ YACS/master/master.py
@@ -5,8 +5,6 @@ import json
 import time
 import queue
 
-a = 0
-
 logging.basicConfig(filename="newfile.log",
                     format='%(asctime)s %(message)s',
                     filemode='w',
@@ -16,38 +14,58 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
 lock = threading.Lock()
+map_tasks = []
+reduce_tasks = []
+dependency = []
+
+def parse_request(request):
+    job = json.loads(request)
+    print()
+    print(f"JOB_ID: {job['job_id']}")
+    print()
+
+def schedule(msg):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(('localhost', 4001))
+        print(f"SENDING WORKER the job {msg}")
+        s.sendall(bytes(msg, 'utf-8'))
+
+def listen_for_worker_updates(lock):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('localhost', 5001))
+        s.listen(5)
+        while True:
+            worker_socket, address = s.accept()
+            print(f"Connection from {address} to port 5001(worker)  has been established.")
+            msg = worker_socket.recv(1024).decode("utf-8")
+            print(msg)
 
 
-def listen_for_jobs():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('localhost', 5000))
-    s.listen(5)
-    while True:
-        clientsocket, address = s.accept()
-        print(f"Connection from {address} has been established.")
-        msg = clientsocket.recv(1024)
-        print(msg.decode("utf-8"))
+def listen_for_jobs(lock):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('localhost', 5000))
+        s.listen(5)
+        while True:
+            request_socket, address = s.accept()
+            print(f"Connection from {address} to 5000(requests) has been established.")
+            msg = request_socket.recv(1024).decode("utf-8")
+            print(f"MASTER RECEIVED JOB:{msg}")
+            parse_request(msg)
+            schedule(msg)
 
 
-listen_for_jobs()
+def main():
+    t1 = threading.Thread(target=listen_for_jobs, args=(lock,))
+    t2 = threading.Thread(target=listen_for_worker_updates, args=(lock,))
 
-def inc_by_10(lock):
-    global a
-    logging.info(a)
-    for i in range(100000):
-        with lock:
-            a += 1
+    t1.start()
+    t2.start()
 
+    t1.join()
+    t2.join()
 
-t1 = threading.Thread(target=inc_by_10, args=(lock,))
-t2 = threading.Thread(target=inc_by_10, args=(lock,))
-
-t1.start()
-t2.start()
-
-t1.join()
-t2.join()
-
-print(a)
-
+if __name__ == "__main__":
+    main()
 
--- YACS/worker/worker.py
+++ YACS/worker/worker.py
@@ -2,3 +2,31 @@ import threading
 import socket
 import logging
 import json
+import sys
+
+def listen_for_task_launch(port, worker_id):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('localhost', port))
+        s.listen(5)
+        while True:
+            master, address = s.accept()
+            msg = master.recv(1024).decode("utf-8")
+            print(msg)
+
+def send_task_updates():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(('localhost', 5000))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+        print('Received', repr(data))
+
+def main():
+    port, worker_id = list(map(int, [sys.argv[1], sys.argv[2]]))
+    listen_for_task_launch(port, worker_id)
+    t1 = threading.Thread(target=send_task_updates)
+    t2 = threading.Thread(target=listen_for_task_launch, args=(port, worker_id))
+
+
+if __name__ == "__main__":
+    main()
